b0c13a07651d8d70a70b292476b4a51c342bcae4
Keyrice <clerymenuel@gmail.com>
2023-03-03 04:32:59 +0100

    conf_ip_windows_Modified
    nouvelle_fonctionnalité

--- conf/conf_ip_windows.py
+++ conf/conf_ip_windows.py
@@ -4,22 +4,29 @@ Voici un exemple de script Python qui définit une adresse IP statique sur un sy
 import subprocess
 
 # Définir l'adresse IP statique, le masque de sous-réseau et la passerelle par défaut
-test = input("tout va bien ? ")
-ip_address = "192.168.1.60"
-subnet_mask = "255.255.255.0"
-default_gateway = "192.168.1.254"
+ip_address = input("Entrez une adresse IP (Ex : 192.168.1.60) : ")
+subnet_mask = input("Entrez un masque de sous réseau (Ex : 255.255.255.0) : ")
+default_gateway = input("Entrez la passerelle par default (Ex : 192.168.1.254) : ")
+dns_server = input("Entrez le DNS (Ex : 8.8.8.8) : ")
+#-------------------------BETA-------------------------
+#network_interface = input("Entrez l'interface réseau (Ex : Ethernet) : ") 
 
 # Configurer l'adresse IP statique avec netsh
+#-------------------------BETA-------------------------
+#command = f"netsh interface ipv4 set address name=\"{network_interface}\" static {ip_address} {subnet_mask} {default_gateway}"
+#subprocess.call(command, shell=True)"""
 command = f"netsh interface ipv4 set address name=\"Ethernet\" static {ip_address} {subnet_mask} {default_gateway}"
 subprocess.call(command, shell=True)
 
 # Configurer le DNS avec netsh
-dns_server = "8.8.8.8"
 command = f"netsh interface ipv4 add dnsserver name=\"Ethernet\" address={dns_server} index=1"
 subprocess.call(command, shell=True)
 
 # Redémarrer le service DHCP pour éviter les conflits d'adresse IP
 subprocess.call("net stop dhcp & net start dhcp", shell=True)
+
+# Affichage de fin
+input("FINI...")
 
 """
 Ce script utilise la bibliothèque subprocess pour exécuter des commandes système et configurer l'adresse IP statique avec la commande netsh. 
