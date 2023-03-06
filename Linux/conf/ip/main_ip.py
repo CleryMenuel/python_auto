4f9b8ba8fb8f567619f247f04a754cdb38c567f5
Keyrice <clerymenuel@gmail.com>
2023-03-06 12:56:11 +0100

    New features

--- Linux/conf/ip/main_ip.py
+++ Linux/conf/ip/main_ip.py
@@ -9,5 +9,5 @@ if platform.system() == 'Linux':
     if version < '20.04':
         os.system("python scripts/ipstatic_ubuntuinf20.04.py")
 
-else:
-    print("La version du système d'exploitation est supérieure à 20.04. Le script ne sera pas exécuté.")
+    else:
+        print("La version du système d'exploitation est supérieure à 20.04. Le script ne sera pas exécuté.")
