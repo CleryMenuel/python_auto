1e732337a3ff393d446a1696b64ffa71778b1c85
Keyrice <clerymenuel@gmail.com>
2023-03-06 13:27:51 +0100

    main_ip.py

--- Linux/conf/ip/main_ip.py
+++ Linux/conf/ip/main_ip.py
@@ -8,6 +8,7 @@ if platform.system() == 'Linux':
 # Vérifier si la version est inférieure à 20.04
     if version < '20.04':
         os.system("python scripts/ipstatic_ubuntuinf20.04.py")
-
+    elif version >= '20.04':
+        print("PARFAITTTTT")
     else:
         print("La version du système d'exploitation est supérieure à 20.04. Le script ne sera pas exécuté.")
